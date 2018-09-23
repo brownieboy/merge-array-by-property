3ac7a21f18bb17a07cd1c8202485587e7c36b410
Michael Brown <brownieboy@gmail.com>
2018-09-23 20:31:47 +1000

    Getting test data...

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,17 @@ const expect = chai.expect;
 
 const mergeArrayByPropertyValue = require("../dist/index.js");
 
+const characters = [
+    { id: "hansolo", name: "Han Solo", planetId: "020" },
+    { id: "lukeskywalker", name: "Luke Skywalker", planetId: "010" },
+    { id: "c3po", name: "C3PO", planetId: "010" }
+];
+
+const planets = [
+    { id: "010", name: "Tattoine", allegiance: "Neutral" },
+    { id: "020", name: "Corellia", allegiance: "Neutral" }
+];
+
 /*
 var validatorObj = require("../dist/abnacn-validator.js");
 
